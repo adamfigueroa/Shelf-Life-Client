fe7c24e6c0b4986d06aa51e6630d7bcc154cebca
Adam Figueroa <admiralfiggins@gmail.com>
2021-02-09 12:48:37 -0800

    added sort function to list

--- src/components/DashBoardList/DashBoardList.js
+++ src/components/DashBoardList/DashBoardList.js
@@ -9,7 +9,7 @@ class DashBoardList extends Component {
 
   state = {
     error: false,
-    filter: false,
+    sort: '',
   };
 
   componentDidMount() {
@@ -32,62 +32,85 @@ class DashBoardList extends Component {
     const addDays = item.days_until_expire;
     const newDate = moment(item.count_down_date).add(addDays, 'days');
     const countdown = newDate.fromNow();
-    const sortNum = countdown.match(/\d+/);
+    let sortNum = countdown.match(/\d+/);
+    if (sortNum === null) {
+      sortNum = 1;
+    }
     if (countdown.includes('ago')) {
       const iName = item.item_name;
-      const iNameSlice = iName.slice(-1)
+      const iNameSlice = iName.slice(-1);
       if (iNameSlice === 's') {
         return (
-          <p className="expired">
-          {item.item_name} have expired {countdown}
-        </p>
+          <div>
+            <p className="expired">
+              {item.item_name} have expired {countdown}
+            </p>
+          </div>
         );
       } else {
         return (
-          <p className="expired">
-          {item.item_name} has expired {countdown}
-        </p>
+          <div>
+            <p className="expired">
+              {item.item_name} has expired {countdown}
+            </p>
+          </div>
         );
       }
     } else {
       return (
         <div>
           <p className="expireTracker">Expires {countdown}</p>
-          <p className="sortNum hidden">{sortNum}</p>
         </div>
       );
     }
   };
 
   loadItems = () => {
-    const userItems = this.context.items.map((item) => {
-      return (
-        <li className="itemCardLi" id={item.id} key={item.id}>
-          <Link to={`/items/${item.id}`}>
-            <div className="itemCard" title="Click here to see details">
-              <h4>{item.item_name}</h4>
-              {this.calculateCountdownDate(item)}
-            </div>
-          </Link>
-        </li>
-      );
-    });
-    console.log(userItems)
+    const userItems = this.context.items
+      .sort((i1, i2) => {
+        if (this.state.sort === '') return true;
+        const date1 = moment(i1.count_down_date).add(
+          i1.days_until_expire,
+          'days'
+        );
+        const date2 = moment(i2.count_down_date).add(
+          i2.days_until_expire,
+          'days'
+        );
+        const difference = date1 - date2;
+        if (this.state.sort === 'ascending') {
+          return difference;
+        } else return -difference;
+      })
+      .map((item) => {
+        return (
+          <li className="itemCardLi" id={item.id} key={item.id}>
+            <Link to={`/items/${item.id}`}>
+              <div className="itemCard" title="Click here to see details">
+                <h4>{item.item_name}</h4>
+                {this.calculateCountdownDate(item)}
+              </div>
+            </Link>
+          </li>
+        );
+      });
     if (userItems === []) {
       return <li className="noItemsBox">No current items to track</li>;
     } else return userItems;
   };
 
-  sortList = (sortType) => {};
+  handleSortChange = (event) => {
+    this.setState({ sort: event.target.value });
+  };
 
   render() {
     return (
       <div className="dashboardBox">
-        <div className='titleBox'>
+        <div className="titleBox">
           <h2>My Fridge</h2>
-          <form className="filterList">
-            <select name="filter" id="filter">
-              <option value="">Filter by</option>
+          <form className="filterList" onChange={this.handleSortChange}>
+            <select name="sort" id="sort">
+              <option value="">Sort by</option>
               <option value="ascending">Ascending</option>
               <option value="descending">Descending</option>
             </select>
